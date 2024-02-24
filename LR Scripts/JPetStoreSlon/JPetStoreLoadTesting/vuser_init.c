d23fe84a64fb15a9ed9debf0ac858ba2a66afe0e
indugaor@ust.com <185240@ust.com>
2024-02-24 20:19:23 +0530

    commit

---
+++ LR Scripts/JPetStoreSlon/JPetStoreLoadTesting/vuser_init.c
@@ -0,0 +1,26 @@
+/* -------------------------------------------------------------------------------
+	Script Title       : 
+	Script Description : 
+                        
+                        
+	Recorder Version   : 0
+   ------------------------------------------------------------------------------- */
+
+vuser_init()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_set_sockets_option("TLS_SNI", "0");
+
+	web_url("Catalog.action", 
+		"URL=https://petstore.octoperf.com/actions/Catalog.action", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
---
+++ LR Scripts/JPetStoreSlon/JPetStoreSomkeTesting/vuser_init.c
@@ -0,0 +1,15 @@
+vuser_init()
+{
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_url("Catalog.action", 
+		"URL=https://petstore.octoperf.com/actions/Catalog.action", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
